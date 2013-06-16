9b52809e0a9c9fd3a752e81c3ea20900d046e59d
luizrogeriocn <luizrogeriocn@gmail.com>
2013-06-16 14:43:30 -0300

    refatorando

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,7 @@ from google.appengine.api import mail
 from google.appengine.api import capabilities
 from google.appengine.api import urlfetch
 
+from register import Register
 from mailer import Mailer
 import urllib
 
@@ -12,29 +13,13 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
 
-    def register(self):
-        #cadastro bolado no lookup
-        form_fields = {
-        "endpoint": "roger-pd-app.appspot.com",
-        "id": "roger-pd-app"
-        }
-
-        url = "http://lookuppd.appspot.com/objects/add"
-            
-        form_data = urllib.urlencode(form_fields)
-
-        result= urlfetch.fetch(url=url,
-        payload=form_data,
-        method=urlfetch.POST,
-        headers={'Content-Type': 'application/x-www-form-urlencoded'})
-
     def get(self):
         user = users.get_current_user()
         if user:
 
             Mailer.mail(self, user)
 
-            self.register()
+            Register.register(self)
 
         else:
             self.redirect(users.create_login_url(self.request.uri))
---
+++ register.py
@@ -0,0 +1,21 @@
+from google.appengine.api import urlfetch
+import urllib
+
+class Register():
+
+	@staticmethod
+	def register(self):
+		#cadastro bolado no lookup
+		form_fields = {
+		"endpoint": "roger-pd-app.appspot.com",
+		"id": "roger-pd-app"
+		}
+
+		url = "http://lookuppd.appspot.com/objects/add"
+            
+		form_data = urllib.urlencode(form_fields)
+
+		result= urlfetch.fetch(url=url,
+		payload=form_data,
+		method=urlfetch.POST,
+		headers={'Content-Type': 'application/x-www-form-urlencoded'})
